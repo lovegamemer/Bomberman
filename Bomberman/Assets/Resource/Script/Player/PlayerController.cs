4c20869a6bd0045f1707ba38318a229610e69bf7
Poi <o.onepiece9@gmail.com>
2022-04-18 16:16:33 +0700

    [ADD] Add Player Movement Controller

---
+++ Bomberman/Assets/Resource/Script/Player/PlayerController.cs
@@ -0,0 +1,54 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem; 
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float moveSpeed = 5f;
+    public PlayerInputActions playerControls; 
+
+    Vector2 moveDirection = Vector2.zero;
+    private InputAction move;
+    private InputAction fire;
+
+
+    private void Awake()
+    {
+        playerControls = new PlayerInputActions();
+    }
+
+    private void OnEnable()
+    {
+        move = playerControls.Player.Move;
+        move.Enable();
+
+        fire = playerControls.Player.Fire;
+        fire.Enable();
+        fire.performed += Fire;
+    }
+
+    private void OnDisable()
+    {
+        move.Disable();
+        fire.Disable();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        moveDirection = move.ReadValue<Vector2>();
+        
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(moveDirection.x * moveSpeed, moveDirection.y * moveSpeed);
+    }
+
+    private void Fire(InputAction.CallbackContext context)
+    {
+        Debug.Log("Fire");
+    }
+}
